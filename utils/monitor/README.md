2cc5982f8b865559790511179c453ce6249af09a
Federico Borello <156438142+fborello-lambda@users.noreply.github.com>
2024-06-28 19:19:12 -0300

    refactor(monitor): add new configuration method (#19)
    * [WIP]refactor(monitor): change how the way of configuring the script
    
    * refactor(monitor.ts): change the configuration method
    
    add instructions on how to use the systemd service
    
    * feat(checkConnection): add new log messages
    
    * fix: comment

--- utils/monitor/README.md
+++ utils/monitor/README.md
@@ -2,14 +2,14 @@
 
 ## How
 
-1. Create and then configure the `.env`:
+1. Create and then configure the `config.json` file:
 
 ```sh
-cd /home/admin/zksync_stack/utils/monitor
-cp .env.example .env
+cd zksync_stack/utils/monitor
+cp config_example.json config.json
 ```
 
-Fill the .env file with the required values.
+Fill the file with the required values.
 
 2. Setup systemd Service:
 
@@ -17,18 +17,47 @@ Fill the .env file with the required values.
 make setup
 ```
 
-> [!NOTE]
-> If this error is thrown: `Job for monitor.service failed because of unavailable resources or another system error.`
-> It may mean that the `.env` file is missing.
-
-2. (extra) To print the `console.logs()`:
+- To print the `console.logs()`:
 
 ```sh
 journalctl -f -u monitor.service
 ```
 
-3. For more targets and options, use:
+- For more targets and options, use:
 
 ```sh
 make help
+```
+
+## Advanced SetUp
+
+If we want to monitor different blockchains, we can create different `services`, the recommended way of monitoring multiple nodes is:
+
+1. You will need a different `config_file`, i.e. `config_prodchain.json`
+   1. Fill the file with the required values.
+   2. Ideally, the `slack_webhook_url` should be from another Slack's App in order to differentiate between the different monitor services.
+
+2. Create a new `.service` file, i.e. `monitor_prodchain.service`:
+
+```sh
+cp monitor.service monitor_prodchain.service
+```
+
+2. Change the `Description` of the service:
+
+```
+[Unit]
+Description=monitor_prodchain
+```
+
+3. Change the `ExecStart` of the service to import the required `config_file`, in this example `./config_prodchain.json`:
+
+```
+ExecStart=/bin/bash -c '/home/admin/.bun/bin/bun i && /home/admin/.bun/bin/bun run monitor.ts --configFile ./config_prodchain.json'
+```
+
+4. Finally, use the `Makefile` to `setup` this new monitor.
+
+```sh
+make setup SERVICE=monitor_prodchain
 ```
--- utils/monitor/monitor.ts
+++ utils/monitor/monitor.ts
@@ -2,10 +2,45 @@ import { IncomingWebhook } from "@slack/webhook";
 import { Wallet, Provider, utils } from "zksync-ethers";
 import * as ethers from "ethers";
 import { env } from "process";
+import { parseArgs } from "util";
+
+const values = parseArgs({
+    args: Bun.argv,
+    options: {
+        configFile: {
+            type: 'string',
+        },
+    },
+    strict: true,
+    allowPositionals: true,
+}).values;
+
+
+// ################ CONFIG ################
+let configFile = values.configFile || "./config.json"
+const config = require(configFile);
+interface Account {
+    addr: string;
+    descr?: string;
+}
+
+interface Config {
+    slack_webhook_url: string;
+    explorer_url?: string;
+    rpc_url: {
+        l1: string;
+        l2: string;
+    };
+    accounts: Account[];
+    sleep_checkBalance_minutes: number;
+    sleep_checkBlock_ms: number;
+}
+
+const CFG = config as Config;
+// ################ CONFIG ################
 
 // ################ SLACK ################
-const url = process.env.SLACK_WEBHOOK_URL;
-const slack = new IncomingWebhook(url!);
+const slack = new IncomingWebhook(CFG.slack_webhook_url);
 
 const log = async (msg: string) => {
     slack.send({
@@ -15,27 +50,24 @@ const log = async (msg: string) => {
 // ################ SLACK ################
 
 // ################ VARIABLES ################
-const L1_RPC_ENDPOINT = env.L1_RPC_URL || "http://127.0.0.1:8545";
-const L2_RPC_ENDPOINT = env.L2_RPC_URL || "http://127.0.0.1:3050";
-console.log(L1_RPC_ENDPOINT);
-console.log(L2_RPC_ENDPOINT);
+const sleep_checkBalance_minutes = CFG.sleep_checkBalance_minutes * 60e3 || 1 * 60e3;
+const sleep_checkBlock_ms = CFG.sleep_checkBlock_ms || 10e3;
 
-const SLEEP_MS = Number(env.SLEEP_MS) || 10000;
+const explorerUrl = CFG.explorer_url || "explorerUrlPlaceholder";
 
-const L1_ADDR = env.L1_ADDR || "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049";
-
-const l1provider = new ethers.providers.JsonRpcProvider(L1_RPC_ENDPOINT);
-const l2provider = new Provider(L2_RPC_ENDPOINT);
+const l1provider = new ethers.providers.JsonRpcProvider(CFG.rpc_url.l1);
+const l2provider = new Provider(CFG.rpc_url.l2);
 // ################ VARIABLES ################
 
 
 // ################ CHECKERS ################
 const checkBlock = async () => {
-    let prevBlock = 0
-    let alive = false
+    let prevBlock = 0;
+    let alive = false;
 
     while (true) {
         try {
+            await Bun.sleep(sleep_checkBlock_ms);
             const block = await l2provider.getBlockNumber();
             prevBlock = Number(block);
             break;
@@ -44,62 +76,117 @@ const checkBlock = async () => {
         }
     }
 
-    await Bun.sleep(SLEEP_MS);
+    await Bun.sleep(sleep_checkBlock_ms);
 
     while (true) {
-        let block = await l2provider.getBlockNumber();
+        try {
+            await Bun.sleep(sleep_checkBlock_ms);
+            let block = await l2provider.getBlockNumber();
+            if ((block > prevBlock) && !alive) {
+                alive = true;
+            } else if ((block == prevBlock) && alive) {
+                await log(
+                    `Blockchain not advancing\n` +
+                    `Currently, it is normal behavior. The blockchain was stopped and now it received some transactions\n` +
+                    `Explorer: ${explorerUrl}/block/${block}`);
+                alive = false;
+            }
+            prevBlock = block;
 
-        // The +1 handles the case in which the 
-        // block = n and prevBlock = n-1
-        // If using a sleep period of 10[s] the block should be much greater than prevBlock
-        // not just a difference of 1 block. Also, some test blockchains create a new block after 
-        // l1provider.getBlockNumber();
-        if ((block > prevBlock + 1) && !alive) {
-            await log("Node is up")
-            alive = true
-        } else if ((block == prevBlock) && alive) {
-            await log(`Not advancing; block: ${block} // prevBlock: ${prevBlock}`);
-            alive = false
+        } catch (error) {
+            console.error(`Error fetching block number`);
         }
-
-        prevBlock = block
-        await Bun.sleep(SLEEP_MS);
     }
 }
 
-const checkBalance = async (addr: string) => {
-    let THRESHOLD = [0.5, 2, 5, 10];
+const checkConnection = async () => {
+    const retries = 5;
+    let alive = false;
+    let sendmsg = true;
 
-    let lastBalance = Number(ethers.utils.formatEther(await l1provider.getBalance(addr)));
     while (true) {
-
-        let balance = Number(ethers.utils.formatEther(await l1provider.getBalance(addr)));
-
-        for (let i = 0; i < THRESHOLD.length; i++) {
-            if (balance < THRESHOLD[i] && lastBalance >= THRESHOLD[i]) {
-                await log(`Balance of ${addr} is: ${balance}`);
+        for (let i = 0; i < retries; i++) {
+            try {
+                await Bun.sleep(2e3);
+                (await l2provider.getNetwork()).chainId;
+                alive = true;
+                sendmsg = true;
                 break;
-            }
-            else if (balance >= THRESHOLD[i] && lastBalance < THRESHOLD[i]) {
-                await log(`Balance of ${addr} increased to: ${balance}`);
-                break;
+            } catch (e) {
+                console.error(`Error fetching chainId`);
             }
         }
 
-        lastBalance = balance;
-
-        await Bun.sleep(SLEEP_MS);
+        if (!alive && sendmsg) {
+            log(
+                `Unable to connect after ${retries}\n` +
+                `Blockchain seems to be down, URL: ${CFG.rpc_url.l2}`
+            );
+            sendmsg = false;
+        }
     }
+};
+
+const checkBalance = async (accounts: Account[]) => {
+    let lastBalance: number[] = new Array(accounts.length).fill(0);
+
+    for (let i = 0; i < accounts.length; i++) {
+        lastBalance[i] = Number(ethers.utils.formatEther(await l1provider.getBalance(accounts[i].addr)));
+    }
+
+    while (true) {
+        for (let i = 0; i < accounts.length; i++) {
+            lastBalance[i] = await sendBalanceMsg(accounts[i], lastBalance[i]);
+        }
+
+        await Bun.sleep(sleep_checkBalance_minutes);
+    }
+}
+
+const sendBalanceMsg = async (account: Account, lastBalance: number) => {
+    let THRESHOLD = [0.5, 2, 5, 10];
+    let balance = 0;
+
+    await l1provider.getBalance(account.addr)
+        .then((b) => {
+            balance = Number(ethers.utils.formatEther(b));
+        })
+        .catch((e) => {
+            console.error(`Error fetching  balance from L1 \n${e}`);
+            return
+        });
+
+    console.log(`Balance of ${account.addr}: ${balance}`);
+    for (let i = 0; i < THRESHOLD.length; i++) {
+        if (balance < THRESHOLD[i] && lastBalance >= THRESHOLD[i]) {
+            await log(
+                `${account.addr} (${account.descr})\n` +
+                `Balance decreased: ${balance}\n` +
+                `Explorer: ${explorerUrl}/address/${account.addr}`);
+            break;
+        }
+        else if (balance >= THRESHOLD[i] && lastBalance < THRESHOLD[i]) {
+            await log(
+                `${account.addr} (${account.descr})\n` +
+                `Balance increased: ${balance}\n` +
+                `Explorer: ${explorerUrl}/address/${account.addr}`);
+            break;
+        }
+    }
+    return balance
 }
 // ################ CHECKERS ################
 
 async function main() {
-    await log("The monitor service is up and running")
-    let p1 = checkBalance(L1_ADDR);
+    await log(`The monitor service is up and running, monitoring: ${CFG.rpc_url.l2}`)
+
+    let p1 = checkBalance(CFG.accounts);
 
     let p2 = checkBlock();
 
-    await Promise.all([p1, p2]);
+    let p3 = checkConnection();
+
+    await Promise.all([p1, p2, p3]);
 }
 
 main()
